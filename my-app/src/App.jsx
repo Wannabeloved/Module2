8afb666beadb316489dc92a1e6cbb2f2e8c5e594
Artemy <quzyboy@gmail.com>
2024-09-07 18:05:49 +0300

    Задание#2 реакт

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 import styles from "./App.module.css";
 import { Calculator } from "./components/Calculator";
 import { List } from "./components/List";
+import { Recipes } from "./components/Recipes";
 
 // императивщина... :)
 export const App = () => {
 	return (
 		<div className={styles.container}>
-			<List />
+			<Recipes />
+			{<List /> && null}
 			{<Calculator /> && null}
 		</div>
 	);
