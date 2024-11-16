be6ef89537dac734dbdaf7f52485543748b9de86
Artemy <quzyboy@gmail.com>
2024-11-16 23:07:53 +0300

    Добавлена переадресация пользователя на страницу настроек если игра не начата

--- my-app/src/components/TicTacToe/widgets/Game/index.jsx
+++ my-app/src/components/TicTacToe/widgets/Game/index.jsx
@@ -1,9 +1,16 @@
 import { GameField } from "./GameField/GameField";
 import { StopButton } from "./StopButton";
 
-import styles from "./index.module.css";
+import { store } from "../../store";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
+import styles from "./index.module.css";
 export const Game = () => {
+	const navigate = useNavigate();
+	useEffect(() => {
+		if (!store.getState().isGameOn) navigate("/tictactoe/settings");
+	}, []);
 	return (
 		<>
 			<article>
--- my-app/src/components/TicTacToe/widgets/Information/Information.jsx
+++ my-app/src/components/TicTacToe/widgets/Information/Information.jsx
@@ -10,6 +10,7 @@ export const Information = () => {
 	const { isGameOn } = store.getState();
 	const rerender = useRerender();
 	store.subscribe(rerender);
+	console.log("IS GAME ON:: ", isGameOn);
 
 	return (
 		<InformationLayout
